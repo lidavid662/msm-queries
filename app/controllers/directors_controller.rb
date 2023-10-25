152e7cf85cce82a56820d1a661ab8686e698dd08
lidavid662 <146143936+lidavid662@users.noreply.github.com>
2023-10-25 04:58:40 +0000

    done

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,19 @@
+class DirectorsController < ApplicationController
+  def index
+    render({ :template => "director_templates/list" })
+  end
+  def show
+    the_id = params.fetch("the_id")
+    matching_records = Director.where({ :id => the_id })
+    @the_director = matching_records.at(0)
+    render({ :template => "director_templates/details" })
+  end
+  def junior
+    @youngest_director = Director.order(dob: :desc).first
+    render({ :template => "director_templates/junior" })
+  end
+  def senior
+    @eldest_director = Director.order(dob: :asc).second
+    render({ :template => "director_templates/senior" })
+  end
+end
